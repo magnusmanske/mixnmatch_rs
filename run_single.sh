b642b34c6138be32e84c17cb917595cb3a5b9ef2
Magnus Manske <magnusmanske@googlemail.com>
2024-03-12 13:35:28 +0000

    misc

--- run_single.sh
+++ run_single.sh
@@ -2,7 +2,7 @@
 toolforge jobs delete single
 
 toolforge jobs run --wait --mem 2000Mi --cpu 1 --mount=all --image tool-mix-n-match/tool-mix-n-match:latest \
-	--command "sh -c 'target/release/mixnmatch \"$1\" /data/project/mix-n-match/mixnmatch_rs/config.json' \"$2\"" single
+	--command "sh -c 'target/release/mixnmatch \"$1\" /data/project/mix-n-match/mixnmatch_rs/config.json \"$2\"'" single
 #	-o /data/project/mix-n-match/mixnmatch_rs/single.out -e /data/project/mix-n-match/mixnmatch_rs/single.err \
 
 toolforge jobs logs single
