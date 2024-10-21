79ec62d5e9c91f7eea09706e7daa1f4cc87c6539
Magnus Manske <magnusmanske@googlemail.com>
2024-10-21 18:19:24 +0300

    misc

--- restart.sh
+++ restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 toolforge jobs delete rustbot
 \rm ~/rustbot.*
-toolforge jobs run --mem 4500Mi --cpu 3 --continuous --mount=all \
+toolforge jobs run --mem 4000Mi --cpu 3 --continuous --mount=all \
 	--image tool-mix-n-match/tool-mix-n-match:latest \
 	--command "sh -c 'target/release/mixnmatch server /data/project/mix-n-match/mixnmatch_rs/config.json'" \
 	--filelog -o /data/project/mix-n-match/rustbot.out -e /data/project/mix-n-match/rustbot.err \
