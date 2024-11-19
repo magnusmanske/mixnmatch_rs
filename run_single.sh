0cb9a464943494c9941747b356d041585ca787f7
Magnus Manske <magnusmanske@gmail.com>
2024-11-19 13:09:08 +0000

    misc

--- run_single.sh
+++ run_single.sh
@@ -2,7 +2,7 @@
 toolforge jobs delete single
 
 toolforge jobs run --wait --mem 2000Mi --cpu 1 --mount=all --image tool-mix-n-match/tool-mix-n-match:latest \
-	--command "php8.1 -c /data/project/mix-n-match/mixnmatch_rs/php.ini $1 $2 $3 $3" single
+	--command "php8.1 -c /data/project/mix-n-match/mixnmatch_rs/php.ini $1 $2 $3 $4" single
 #	-o /data/project/mix-n-match/mixnmatch_rs/single.out -e /data/project/mix-n-match/mixnmatch_rs/single.err \
 
 toolforge jobs logs single
