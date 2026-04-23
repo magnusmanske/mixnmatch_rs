208abe8b5443dd308cac6911b07d6d70f0052e03
Magnus Manske <magnusmanske@gmail.com>
2026-04-23 07:14:43 +0000

    misc

--- restart.sh
+++ restart.sh
@@ -4,6 +4,6 @@ toolforge jobs delete rustbot
 PORT=8089 toolforge jobs run --mem 3500Mi --cpu 3 --continuous --port 8089 \
 	--mount=all \
 	--image tool-mix-n-match/tool-mix-n-match:latest \
-	--command "sh -c 'target/release/main server --config /data/project/mix-n-match/mixnmatch_rs/config.json'" \
+	--command "target/release/main server --config /data/project/mix-n-match/mixnmatch_rs/config.json" \
 	--filelog -o /data/project/mix-n-match/rustbot.out -e /data/project/mix-n-match/rustbot.err \
 	rustbot
